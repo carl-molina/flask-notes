5fc24b17d444252f301a804824b86c6dc49f1aff
Carl Molina <carlandrew.molina@gmail.com>
2023-12-12 12:13:38 -0800

    Started on User model

--- models.py
+++ models.py
@@ -7,3 +7,29 @@ db = SQLAlchemy()
 class User(db.Model):
     """User."""
 
+    __tablename__ = "users"
+
+    __table_args__ = (
+        db.UniqueConstraint("name", "description"),
+    )
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    name = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True,
+    )
+
+    description = db.Column(
+        db.Text,
+        nullable=False,
+        default='',
+    )
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} playlist.id={self.id}>"
